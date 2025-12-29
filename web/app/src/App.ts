20b606902e8a9e37b71b11406b67f98a1c20de0b
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2025-12-29 10:58:17 +0100

    [web/app] Remove local storage for unput stream url (use query param instead)

--- web/app/src/App.ts
+++ web/app/src/App.ts
@@ -1,7 +1,5 @@
 import Player from './Player'
 
-const STORAGE_BASE_KEY = 'simid-demo-app:'
-
 const DEFAULT_STREAM_URL = 'https://d2lwku66j7s1id.cloudfront.net/9bf31c7ff062936a7f941ad65712fad3/out/v1/6e0f649095ca4131b16bd0f877048629/index.mpd?nl-config=demo-samples-live'
 
 export default class App {
@@ -37,7 +35,7 @@ export default class App {
     this.streamButtonStop.onclick = (e) => this.stopStream()
 
     const urlParam = (new URL(window.location.href)).searchParams.get('url')
-    const url = urlParam || this.getFromLocalStorage('stream', DEFAULT_STREAM_URL)
+    const url = urlParam || DEFAULT_STREAM_URL
 
     this.streamEditUrl.value = url
   }
@@ -49,28 +47,10 @@ export default class App {
   private async loadStream() {
     const url = this.streamEditUrl.value
     await this.player.load(url)
-    this.saveToLocalStorage('stream', this.streamEditUrl.value)
   }
 
   private async stopStream() {
     await this.player.stop()
-  }
-
-  private getFromLocalStorage(field: string, defaultValue = '') : string {
-    try {
-      return localStorage.getItem(STORAGE_BASE_KEY + field) || defaultValue
-    } catch (e) {
-      console.error(e)
-      return ''
-    }
-  }
-
-  private saveToLocalStorage(field: string, value: string) {
-    try {
-      localStorage.setItem(STORAGE_BASE_KEY + field, value)
-    } catch (e) {
-      console.error(e)
-    }
   }
 
   private setResizeObserver() {
