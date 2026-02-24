e638aea6740514ab27406143d6e38e31fbe23d01
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2026-02-24 16:59:04 +0100

    [web/app] Reactivate media time updates

--- web/app/src/Player.ts
+++ web/app/src/Player.ts
@@ -72,7 +72,7 @@ export default class Player {
     const playerRect: DOMRect = this.getElementDimensions(this.playerContainer)
 
     console.log(`[Player] Load SIMID - uri:${creativeUri} duration:${duration}`)
-    const simidController = new SimidController(playerRect, playerRect, creativeUri, adParameters, duration, false, -1)
+    const simidController = new SimidController(playerRect, playerRect, creativeUri, adParameters, duration, false)
 
     simidController.onGetMediaState = () => this.getMediaState()
     simidController.onAddSimid = (iframe: HTMLIFrameElement) => this.addSimidIframe(adId, iframe)
