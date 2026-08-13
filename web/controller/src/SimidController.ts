ab224ab74719cf99e08dcb3d8f6bba6ddf3e9428
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2026-08-13 09:18:03 +0200

    [web/controller] Add onError jsdoc

--- web/controller/src/SimidController.ts
+++ web/controller/src/SimidController.ts
@@ -204,7 +204,10 @@ export class SimidController extends SimidComponent {
   }
 
   /**
-   * Set the callback function called when an error occured. 
+   * Set the callback function called when an error occured.
+   * @param message the message type that caused/sent the error
+   * @param errorCode the error code
+   * @param errorMEssage the error message
    */
   public set onError(cb: (message: string, errorCode: number, errorMessage: string) => void) {
     this._onError = cb
