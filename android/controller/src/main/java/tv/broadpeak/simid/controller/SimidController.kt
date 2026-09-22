2f3289c22a05a824be264f73eabe58e25e0bc5d8
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2026-09-22 17:11:19 +0200

    Add message callback API to be notified of sent and received SIMID messages (#41)
    * [web/controller] Add callback API to enable application to be notified of sent and received SIMID messages
    
    * [web/app] Use callback API to forward/notify SmartLib of sent/received messages
    
    * [android/controller] Add callback API to enable application to be notified of sent and received SIMID messages
    
    * [android/app] Use callback API to forward/notify SmartLib of sent/received messages
    
    * [android/controller] Fix warning messages
    
    * [ios/controller] Add callback API to enable application to be notified of sent and received SIMID messages
    
    * [ios/app] Use callback API to forward/notify SmartLib of sent/received messages
    
    * [web/controller] export callbacks

--- android/controller/src/main/java/tv/broadpeak/simid/controller/SimidController.kt
+++ android/controller/src/main/java/tv/broadpeak/simid/controller/SimidController.kt
@@ -21,6 +21,18 @@ import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.encodeToJsonElement
 
 /**
+ * Callback function called to be notified of a received SIMID message, for information and debug purpose.
+ * @param message the message a stringified JSON
+ */
+typealias MessageReceivedCallback = (message: String) -> Unit
+
+/**
+ * Callback function called to be notified of a sent SIMID message, for information and debug purpose.
+ * @param message the message a stringified JSON
+ */
+typealias MessageSentCallback = (message: String) -> Unit
+
+/**
  * Callback function called to retrieve current media state.
  * @return the current media state
  */
@@ -125,6 +137,9 @@ public open class SimidController (
     private var _isStopping: Boolean = false
     private var _timerMediaTimeupdate: Job? = null
 
+
+    private var onMessageReceived: MessageReceivedCallback? = null
+    private var onMessageSent: MessageSentCallback? = null
     private var onGetMediaState: GetMediaStateCallback? = null
     private var onPlayMedia: PlayMediaCallback? = null
     private var onPauseMedia: PauseMediaCallback? = null
@@ -143,6 +158,22 @@ public open class SimidController (
     }
 
     //region Callbacks
+    /**
+     * Set the callback function called to be notified of received SIMID messages.
+     * @param cb the callback function
+     */
+    fun onMessageReceived(cb: MessageReceivedCallback) {
+        this.onMessageReceived = cb
+    }
+
+    /**
+     * Set the callback function called to be notified of sent SIMID messages.
+     * @param cb the callback function
+     */
+    fun onMessageSent(cb: MessageSentCallback) {
+        this.onMessageSent = cb
+    }
+
     /**
      * Set the callback function called to retrieve current media state.
      * @param cb the callback function
@@ -283,6 +314,12 @@ public open class SimidController (
     override fun postMessage(message: String) {
         Log.v(TAG, "[SIMID][Player][S]: $message")
 
+        try {
+            onMessageSent?.invoke(message)
+        } catch(e: Exception) {
+            Log.w(TAG, "Sent message handler thrown an exception: $e")
+        }
+
         val script =
             """
             window.originalPostMessage('$message', '*');
@@ -291,6 +328,15 @@ public open class SimidController (
         activity.runOnUiThread {
             webView?.evaluateJavascript(script, null)
         }
+    }
+
+    override fun receiveMessage(messageStr: String) {
+        try {
+            this.onMessageReceived?.invoke(messageStr)
+        } catch(e: Exception) {
+            Log.w(TAG, "Received message handler thrown an exception: $e")
+        }
+        super.receiveMessage(messageStr)
     }
 
     private fun addCreativeMessageListeners() {
